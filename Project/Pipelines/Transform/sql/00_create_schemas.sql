eaa6486128cbeda8bf77c58ba7293490ea6d6bbd
GlideHound <christjiang1492@gmail.com>
2026-05-27 15:03:12 -0400

    Modified 00_create_schemas.sql, deleted cleaned schema, added staging, warehouse and mart schemas

--- Project/Pipelines/Transform/sql/00_create_schemas.sql
+++ Project/Pipelines/Transform/sql/00_create_schemas.sql
@@ -1,4 +1,12 @@
 -- create_schemas.sql
 -- Purpose: create raw and cleaned schemas in the PostgreSql database
+
+--raw: the raw data
+--staging: cleaned and standardized data
+--warehouse: business model fact tables
+--mart: for analytical use
+
 CREATE SCHEMA IF NOT EXISTS raw;
-CREATE SCHEMA IF NOT EXISTS cleaned;
+CREATE SCHEMA IF NOT EXISTS staging;
+CREATE SCHEMA IF NOT EXISTS warehouse;
+CREATE SCHEMA IF NOT EXISTS mart;
