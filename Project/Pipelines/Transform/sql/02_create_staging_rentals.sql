912efd8a41c891a4d2e1acdf3cd8b59ce71a1c17
GlideHound <christjiang1492@gmail.com>
2026-06-10 16:37:29 -0400

    Updated 02_create_staging_rentals.sql, added queries that handles the edge cases

--- Project/Pipelines/Transform/sql/02_create_staging_rentals.sql
+++ Project/Pipelines/Transform/sql/02_create_staging_rentals.sql
@@ -1,14 +1,6 @@
 -- create_staging_rentals.sql
 -- Purpose: clean and standardize the data from raw table
 -- We already handled deduplication of listing_id in API call phase, so don't worry about that
-
--- In the future, check edge case values like rent price = 1
-DROP TABLE IF EXISTS staging.stg_rental_listings;
-
--- create_staging_rentals.sql
--- Purpose: clean and standardize the data from raw table
--- We already handled deduplication of listing_id in API call phase, so don't worry about that
-
 DROP TABLE IF EXISTS staging.stg_rental_listings;
 
 CREATE TABLE staging.stg_rental_listings AS
@@ -26,16 +18,47 @@ WITH cleaned AS (
             WHEN rent_min = 1 THEN NULL
             ELSE rent_min
         END AS rent_min_clean,
+
         CASE
             WHEN rent_max = 1 THEN NULL
             ELSE rent_max
         END AS rent_max_clean,
+
         beds_min,
         beds_max,
+        CASE
+            WHEN beds_min >= 0 THEN beds_min
+            ELSE NULL
+        END AS beds_min_clean,
+
+        CASE
+            WHEN beds_max >= 0 THEN beds_max
+            ELSE NULL
+        END AS beds_max_clean,
+
         bath_min,
         bath_max,
+        CASE
+            WHEN bath_min >= 0 THEN bath_min
+            ELSE NULL
+        END AS bath_min_clean,
+        CASE
+            WHEN bath_max >= 0 THEN bath_max
+            ELSE NULL
+        END AS bath_max_clean,
+
         size_min,
         size_max,
+        CASE
+            WHEN size_min > 0 THEN size_min
+            ELSE NULL
+        END AS size_min_clean,
+
+        CASE
+            WHEN size_max > 0 THEN size_max
+            ELSE NULL
+        END AS size_max_clean,
+
         created_date,
         modified_date,
         LOWER(NULLIF(TRIM(highlight_status), '')) AS highlight_status,
@@ -62,18 +85,20 @@ SELECT
     rent_min_clean,
     rent_max_clean,
     CASE
-        WHEN rent_min = 1 AND rent_max_clean IS NOT NULL
-            THEN rent_max_clean
+        WHEN rent_min = 1 AND rent_max_clean IS NOT NULL THEN rent_max_clean
+        WHEN rent_min_clean IS NOT NULL
+        AND rent_max_clean IS NOT NULL
+        AND rent_min_clean > rent_max_clean THEN NULL
         WHEN rent_min_clean IS NOT NULL 
-         AND rent_max_clean IS NOT NULL 
-         AND rent_min_clean <= rent_max_clean
-            THEN (rent_min_clean + rent_max_clean) / 2.0
+        AND rent_max_clean IS NOT NULL 
+        AND rent_min_clean <= rent_max_clean THEN (rent_min_clean + rent_max_clean) / 2.0
         WHEN rent_min_clean IS NOT NULL
             THEN rent_min_clean
         WHEN rent_max_clean IS NOT NULL
             THEN rent_max_clean
         ELSE NULL
     END AS rent_avg,
+
     CASE
         WHEN rent_min = 1 AND rent_max_clean IS NOT NULL
             THEN 'upper_bound_only'
@@ -89,14 +114,17 @@ SELECT
             THEN 'upper_bound_only'
         ELSE 'unknown'
     END AS rent_bound_type,
+
     CASE
         WHEN rent_min = 1 THEN TRUE
         ELSE FALSE
     END AS rent_min_placeholder_flag,
+
     CASE
         WHEN rent_max = 1 THEN TRUE
         ELSE FALSE
     END AS rent_max_placeholder_flag,
+
     CASE
         WHEN rent_min_clean IS NOT NULL
          AND rent_max_clean IS NOT NULL
@@ -104,30 +132,76 @@ SELECT
             THEN TRUE
         ELSE FALSE
     END AS invalid_rent_range,
+
     beds_min,
     beds_max,
+    beds_min_clean,
+    beds_max_clean,
     CASE 
-        WHEN beds_min IS NOT NULL AND beds_max IS NOT NULL THEN (beds_min + beds_max) / 2.0
-        WHEN beds_min IS NOT NULL THEN beds_min
-        WHEN beds_max IS NOT NULL THEN beds_max
+        WHEN beds_min_clean IS NOT NULL 
+        AND beds_max_clean IS NOT NULL 
+        AND beds_min_clean > beds_max_clean THEN NULL
+        WHEN beds_min_clean IS NOT NULL 
+        AND beds_max_clean IS NOT NULL 
+        AND beds_min_clean <= beds_max_clean
+        THEN (beds_min_clean + beds_max_clean) / 2.0
+        WHEN beds_min_clean IS NOT NULL THEN beds_min_clean
+        WHEN beds_max_clean IS NOT NULL THEN beds_max_clean
         ELSE NULL
     END AS beds_avg,
+
+    CASE
+        WHEN beds_min_clean IS NOT NULL
+        AND beds_max_clean IS NOT NULL
+        AND beds_min_clean > beds_max_clean THEN TRUE
+        ELSE FALSE
+    END AS invalid_beds_range,
     bath_min,
     bath_max,
+    bath_min_clean,
+    bath_max_clean,
     CASE 
-        WHEN bath_min IS NOT NULL AND bath_max IS NOT NULL THEN (bath_min + bath_max) / 2.0
-        WHEN bath_min IS NOT NULL THEN bath_min
-        WHEN bath_max IS NOT NULL THEN bath_max
+        WHEN bath_min_clean IS NOT NULL 
+        AND bath_max_clean IS NOT NULL 
+        AND bath_min_clean > bath_max_clean THEN NULL
+        WHEN bath_min_clean IS NOT NULL 
+        AND bath_max_clean IS NOT NULL 
+        AND bath_min_clean <= bath_max_clean THEN (bath_min_clean + bath_max_clean) / 2.0
+        WHEN bath_min_clean IS NOT NULL THEN bath_min_clean
+        WHEN bath_max_clean IS NOT NULL THEN bath_max_clean
         ELSE NULL
     END AS bath_avg,
+    
+    CASE
+        WHEN bath_min_clean IS NOT NULL
+        AND bath_max_clean IS NOT NULL
+        AND bath_min_clean > bath_max_clean THEN TRUE
+        ELSE FALSE
+    END AS invalid_bath_range,
+
     size_min,
     size_max,
+    size_min_clean,
+    size_max_clean,
     CASE
-        WHEN size_min IS NOT NULL AND size_max IS NOT NULL THEN (size_min + size_max) / 2.0
-        WHEN size_min IS NOT NULL THEN size_min
-        WHEN size_max IS NOT NULL THEN size_max
+        WHEN size_min_clean IS NOT NULL 
+        AND size_max_clean IS NOT NULL
+        AND size_min_clean > size_max_clean THEN NULL
+        WHEN size_min_clean IS NOT NULL 
+        AND size_max_clean IS NOT NULL
+        AND size_min_clean <= size_max_clean THEN (size_min_clean + size_max_clean) / 2.0
+        WHEN size_min_clean IS NOT NULL THEN size_min_clean
+        WHEN size_max_clean IS NOT NULL THEN size_max_clean
         ELSE NULL
     END AS size_avg,
+
+    CASE
+        WHEN size_min_clean IS NOT NULL 
+        AND size_max_clean IS NOT NULL
+        AND size_min_clean > size_max_clean THEN TRUE
+        ELSE FALSE
+    END AS invalid_size_range,
+
     created_date,
     modified_date,
     highlight_status,
@@ -136,10 +210,10 @@ SELECT
     verified,
     loaded_at,
     (rent_min_clean IS NOT NULL OR rent_max_clean IS NOT NULL) AS has_rent,
-    (size_min IS NOT NULL OR size_max IS NOT NULL) AS has_size,
     (longitude IS NOT NULL AND latitude IS NOT NULL) AS has_location,
-    (beds_min IS NOT NULL OR beds_max IS NOT NULL) AS has_beds,
-    (bath_min IS NOT NULL OR bath_max IS NOT NULL) AS has_baths
+    (size_min_clean IS NOT NULL OR size_max_clean IS NOT NULL) AS has_size,
+    (beds_min_clean IS NOT NULL OR beds_max_clean IS NOT NULL) AS has_beds,
+    (bath_min_clean IS NOT NULL OR bath_max_clean IS NOT NULL) AS has_baths
 FROM cleaned
 WHERE rent_min_clean IS NOT NULL 
    OR rent_max_clean IS NOT NULL;
